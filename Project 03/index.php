7156c3cba86b678f4594b3541daf8691f642da85
sohelbd <shlskr300@gmail.com>
2022-02-18 17:02:01 +0600

    first commit

---
+++ Project 03/index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<title>new project</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="ddsmoothmenu.css">
+</head>
+
+<body><br><br>
+<div class=o>
+<a href=#>Home&nbsp;&nbsp;</a>
+<a href=#>Service&nbsp;&nbsp;</a>
+<a href=#>About&nbsp;&nbsp;</a>
+<a href=#>Publish&nbsp;&nbsp;</a>
+<a href=#>Feedback&nbsp;&nbsp;</a>
+<a href=#>contacts&nbsp;&nbsp;</a>
+<a id=o1 href=#>Accounts&nbsp;&nbsp;</a><br>
+</div>
+
+<div class=a1></div><br>
+<div class=b>
+<img src=images/str.jpg><p>my name is sohel khn<br>my name is sohel khn<br>my name is sohel khn<br>my nme is sohel khn</p>
+<img src=images/msg.jpg><p>my name is sohel khn<br>my name is sohel khn<br>my name is sohel khn<br>my nme is sohel khn</p>
+<img src=images/hme.jpg><p>my name is sohel khn<br>my name is sohel khn<br>my name is sohel khn<br>my nme is sohel khn</p></div><br>
+
+<div class=c><div class=c1><h3><font color=white>About us</font></h3>
+<p><font color=white>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul</font></p></div>
+<div class=c1><h3><font color=white>our products</font></h3>
+<p><font color=white>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul</font></p></div>
+<div class=c1><h3><font color=white>our products</font></h3>
+<p><font color=white>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul<br>my nme is Mohammed Nurul</font></p></div>
+</div><br>
+<br>
+<div class=footer>
+<ul><li><a href="products.html">About&nbsp;&nbsp;</a>
+                    <ul>
+                        <li><a href="#">Sub menu 1</a></li>
+                        <li><a href="#">Sub menu 2</a></li>
+                        <li><a href="#">Sub menu 3</a></li>
+                        <li><a href="#">Sub menu 4</a></li>
+                        <li><a href="#">Sub menu 5</a></li>
+                  </ul>
+                </li>
+
+<li><a href=#>Support&nbsp;&nbsp;</a></li>
+<li><a href=#>Contacts&nbsp;&nbsp;</a></li>
+<li><a id=o1 href=#>Faq&nbsp;&nbsp;</a></li>
+</ul>
+</div><br>
+
+</body>
+</html>
+
+<!---A Q !
+q a z
+
+my job sheduling:
+psd-html-wordpress---!>
